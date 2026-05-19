2a46efbfbe458d53d27cb639600d4b4343e15cc3
Sak <st25622@ormiston.school.nz>
2026-05-20 11:19:37 +1200

    changes

--- app.py
+++ app.py
@@ -1,28 +1,17 @@
+from flask import Flask, render_template
 import json
 
-from flask import Flask, render_template, request, redirect, url_for, session, flash 
-
 app = Flask(__name__)
-app.secret_key = 'your secret key'
 
+def load_data():
+    with open("data/flowers.json") as file:
+        flowers = json.load(file)
+    return flowers
 
-@app.route('/')
-def index():
-    # flowers = load_data()
-    return render_template('index.html')
-    # return render_template('index.html', flowers=flowers)
+@app.route("/")
+def home():
+    flowers = load_data()
+    return render_template("index.html", flowers=flowers)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/checkout')
-def checkout():
-    return render_template('invoice.html')
-
-@app.route('/orders')
-def order_history():
-    return render_template('order_history.html')
-
-if __name__ == '__main__': 
+if __name__ == "__main__":
     app.run(debug=True)
